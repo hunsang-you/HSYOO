58f6947461ea76a30a68cdfcc9711476333802c8
hunsang-you <51877128+hunsang-you@users.noreply.github.com>
2021-07-16 15:28:28 +0900

    2021-07-16 15:30

---
+++ java/First/src/lesson00/E002_ArithmeticOperator.java
@@ -0,0 +1,45 @@
+package lesson00;
+
+public class E002_ArithmeticOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//
+//		int a = 10;
+//		int b = 20;
+		
+//		int c;
+//		c = a + b;
+//		
+//		System.out.println(a + "+" + b + "=" + (a+b));
+		
+		 // declare variables
+	    int a = 12, b = 5;
+
+	    // addition operator
+	    System.out.println("a + b = " + (a + b));
+
+	    // subtraction operator
+	    System.out.println("a - b = " + (a - b));
+
+	    // multiplication operator
+	    System.out.println("a * b = " + (a * b));
+
+	    // division operator
+	    System.out.println("a / b = " + (a / b));
+
+	    // modulo operator
+	    System.out.println("a % b = " + (a % b));
+	    
+	    
+	    
+	    System.out.println("===================");
+	    
+	    
+	    System.out.println(9 / 2);
+	    System.out.println(9.0 / 2);
+	    System.out.println(9 / 2.0);
+	    System.out.println(9.0 / 2.0);
+	}
+
+}
---
+++ java/First/src/lesson00/E003_AssignmentOperator.java
@@ -0,0 +1,27 @@
+package lesson00;
+
+public class E003_AssignmentOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		// create variables
+	    int a = 4;
+	    int var;
+
+	    // assign value using =
+	    var = a;
+	    System.out.println("var using =: " + var);
+
+	    // assign value using =+
+	    var += a;
+	    System.out.println("var using +=: " + var);
+
+	    // assign value using =*
+	    var *= a;
+	    System.out.println("var using *=: " + var);
+		
+	}
+
+}
