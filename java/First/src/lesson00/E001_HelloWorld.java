a3bf172cd273f9f603c60d477c40a23d0739ec74
hunsang-you <51877128+hunsang-you@users.noreply.github.com>
2021-07-16 10:07:03 +0900

    Initial commit

---
+++ java/First/src/lesson00/E001_HelloWorld.java
@@ -0,0 +1,9 @@
+package lesson00;
+
+public class E001_HelloWorld {
+
+	public static void main(String[] arg) {
+		
+		System.out.println("Hi, Java oO01Il");
+	}
+}
---
+++ python/PycharmProjects/prj001/venv/first.py
@@ -0,0 +1,3 @@
+birthYear = int(input())
+age = 2021 - birthYear
+print("당신의 나이는 " + str(age) + "살입니다.")
